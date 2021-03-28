f1d03088f032dc92e2a9c7b73fc8f1c8d2a6cd5b
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-03-28 22:28:54 +0200

    I continued to learn redux

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Like from "./Like";
 import {isEmpty} from "./Utils";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import {editPost, deletePost} from "../actions/post.action";
 
 const Post = ({ post }) => {
 
-  const user = useSelector((state)=> state.userReducer)
+  const [editToggle, setEditToggle] = useState(false);
+  const [editContent, setEditContent] = useState(post.content);
+  const user = useSelector((state)=> state.userReducer);
+  const dispatch = useDispatch();
+
+  const handleEdit=(element)=>{
+    element.preventDefault();
+    
+    const postData = {
+      title: post.title,
+      author: user[0].pseudo,
+      content: editContent,
+      likes: post.likes,
+      id: post.id
+    }
+
+    dispatch(editPost(postData))
+    setEditToggle(false);
+
+  }
 
 
   return (
     <div className="post">
       {!isEmpty(user[0]) && user[0].pseudo === post.author &&(
         <div className="edit-delete">
-        <img src="../../public/icons/edit.svg" alt="edit" />
-        <img src="../../public/icons/delete.svg" alt="delete" />
+        <img src="./icons/edit.svg" alt="edit" onClick={(e)=>{setEditToggle(!editToggle)}}/>
+        <img src="./icons/delete.svg" alt="delete" onClick={()=>{dispatch(deletePost(post.id))}} />
         </div>
       )}
       <h2>{post.title}</h2>
@@ -22,8 +42,14 @@ const Post = ({ post }) => {
         className="post-img"
         alt="img-post"
       />
-
-      <p>{post.content}</p>
+      {editToggle ?(
+                  <form onSubmit={e => handleEdit(e)}>
+                    <textarea defaultValue={post.content} onChange={e=>setEditContent(e.target.value)}></textarea>
+                    <input type="submit" value="Accept modification"/>
+                  </form>
+      ): 
+      (<p>{post.content}</p>)}
+      
 
       <div className="author">
         <h5>{post.author}</h5>
--- src/reducers/post.reducer.js
+++ src/reducers/post.reducer.js
@@ -1,4 +1,4 @@
-import { GET_POSTS, ADD_POSTS } from "../actions/post.action";
+import { GET_POSTS, ADD_POSTS, EDIT_POST, DELETE_POST, ADD_LIKE } from "../actions/post.action";
 
 const initialState = {};
 
@@ -8,6 +8,30 @@ export default function postReducer(state = initialState, action){
             return action.payload;
         case ADD_POSTS:
             return [action.payload, ...state];
+        case EDIT_POST:
+            return(state.map((post)=>{
+                if(post.id === action.payload.id){
+                    return{
+                        ...post,
+                        content: action.payload.content
+                    }
+                }else{
+                    return post
+                }
+            }))
+        case DELETE_POST:
+            return state.filter((post) => post.id !== action.payload.postId);
+        case ADD_LIKE:
+            return (state.map((post)=>{
+                if(post.id === action.payload.id){
+                    return {
+                        ...post,
+                        likes: action.payload.likes
+                    }
+                }else{
+                    return post;
+                }
+            }))
         default:
             return state;
     }
